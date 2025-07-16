d2738382c59c7a0bded6d4a01060d1cc8a60243f
j-li928 <jessicajli507@gmail.com>
2025-07-15 23:27:46 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("JavaScript is working!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    const parallaxElement = document.querySelector('.parallax-collage');
+    
+    if (parallaxElement && window.innerWidth <= 768) {
+        const backgroundDiv = document.createElement('div');
+        backgroundDiv.style.cssText = `
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-image: url('https://j-li928.github.io/j-li928/collage.jpg');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            z-index: -1;
+        `;
+        
+        parallaxElement.style.position = 'relative';
+        parallaxElement.appendChild(backgroundDiv);
+        
+        parallaxElement.style.backgroundImage = 'none';
+        
+        window.addEventListener('scroll', function() {
+            const scrolled = window.pageYOffset;
+            const rate = scrolled * -0.3;
+            backgroundDiv.style.transform = `translateY(${rate}px)`;
+        });
+    }
+});
+
